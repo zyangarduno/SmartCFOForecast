442358bdfba07262a1fed3510a690652cf10b0ff
Zyanya Garduno <zyanya_garduno@outlook.com>
2026-02-27 14:22:06 -0600

    Create config.py for data and sheet URLs
    Added configuration for raw data directory and sales files.

---
+++ src/config.py
@@ -0,0 +1,20 @@
+# src/config.py
+RAW_DIR = "data/raw"
+
+FORECAST_SHEET_URL = "https://docs.google.com/spreadsheets/d/1x-s-VOmCF4sKu3sNJeVlfmwtU-vdxyU8/edit"
+INVENTARIO_SHEET_URL = "https://docs.google.com/spreadsheets/d/1ew0Luxc_76Xa4tWO7Y52AeIwll7jxtuj/edit"
+
+SALES_FILES = {
+    "ventas_2025_01.csv": "18K4q4CYM0ECjAJxbLqfCfUEub45qd7yb",
+    "ventas_2025_02.csv": "1eL0YS47mi9_cY8McbdGeXpESe4Y6K89B",
+    "ventas_2025_03.csv": "1-2ArxY8CZS17E4c-iUQHZR01ji9f4B7W",
+    "ventas_2025_04.csv": "1H7IKgL8mz8tIbg-Ryb_uL1oZ4pR4r4v9",
+    "ventas_2025_05.csv": "1yOwCYbKv6Qmv0LbfnMiMAhgwfxcdGgSc",
+    "ventas_2025_06.csv": "1V2SfYUfiZ6NM9enHZzkW_vosE1G93m8L",
+    "ventas_2025_07.csv": "15_AGTJYzUkxs5DBKk2Uof53OD3H_1SbT",
+    "ventas_2025_08.csv": "1VnkQa2vVjFOWo-xLbMGP-bjsdIuNXogZ",
+    "ventas_2025_09.csv": "1bJwrUIQLZkBNymFnkLaaMyarm9QaGPFE",
+    "ventas_2025_10.csv": "16fIiRuF9QD6QZPQdKhnXgWPieCugB9KE",
+    "ventas_2025_11.csv": "19_oydmbQkhxVhAWLX_WkQoxXBDBf6TH6",
+    "ventas_2025_12.csv": "1_pDSxHRy6GI_A8vM87qAHyOC56JBN7Le",
+}
